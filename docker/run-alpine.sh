4f652d414e1d1cf3cdb12b14f399ed48c8424f08
baardl <bard.lind@gmail.com>
2019-09-28 16:39:11 +0200

    #1 Built docker image and can be run locally on Ubuntu

--- docker/run-alpine.sh
+++ docker/run-alpine.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run -it --name=bacnet-azure-iot-java <your docker user>/bacnet-azure-iot-java:alpine /bin/bash
+#docker run -it --name=bacnet-azure-iot-java <your docker user>/bacnet-azure-iot-java:alpine /bin/bash
+docker run --name=bacnet-azure-iot-java <your docker user>/bacnet-azure-iot-java:alpine
