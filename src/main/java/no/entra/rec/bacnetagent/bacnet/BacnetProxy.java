3d48d8fbe4497cfcc5727af57d7957a2acf4a92a
baardl <bard.lind@entra.no>
2019-10-31 10:36:59 +0100

    #5 Constructor for defining address to bind agent to.

--- src/main/java/no/entra/rec/bacnetagent/bacnet/BacnetProxy.java
+++ src/main/java/no/entra/rec/bacnetagent/bacnet/BacnetProxy.java
@@ -6,16 +6,24 @@ import org.code_house.bacnet4j.wrapper.api.Property;
 import org.code_house.bacnet4j.wrapper.ip.BacNetIpClient;
 import org.slf4j.Logger;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Set;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class BacnetProxy {
     private static final Logger log = getLogger(BacnetProxy.class);
+    private static final int BACNET_AGENT_DEVICE_ID = 2001;
     private final BacNetIpClient client;
 
     public BacnetProxy(BacNetIpClient client) {
         this.client = client;
+    }
+
+    public BacnetProxy(InetAddress bacnetAgentAddress) {
+        client = new BacNetIpClient(bacnetAgentAddress.getHostAddress(), "255.255.255.255", BACNET_AGENT_DEVICE_ID);
+
     }
 
     public void discoverAndReport() {
@@ -39,10 +47,12 @@ public class BacnetProxy {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
         int clientDeviceId = 2001;
-        BacNetIpClient client = new BacNetIpClient("192.168.1.31", "255.255.255.255", clientDeviceId);
-        BacnetProxy proxy = new BacnetProxy(client);
+        //BacNetIpClient client = new BacNetIpClient("192.168.1.31", "255.255.255.255", clientDeviceId);
+        //BacnetProxy proxy = new BacnetProxy(client);
+        InetAddress bacnetAgentAddress = InetAddress.getByName("192.168.1.31");
+        BacnetProxy proxy = new BacnetProxy(bacnetAgentAddress);
         proxy.discoverAndReport();
 
     }
